8dfbea71d4537e05d1536cf3839e44c0e0db0903
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2022-05-10 23:34:49 -0300

    feat: configuring email sending

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import nodemailer from "nodemailer";
 import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "e08744e36aaf1c",
+    pass: "4525201deaee33",
+  },
+});
 
 app.post("/feedbacks", async (req, res) => {
   const { types, comment, screenshot } = req.body;
@@ -14,6 +24,18 @@ app.post("/feedbacks", async (req, res) => {
       comment,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: "equipe Feedget <oi@feedget.com>",
+    to: "Andre Loureiro <contato.loureiro1@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${types}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
   });
 
   return res.status(201).json({ data: feedback });
